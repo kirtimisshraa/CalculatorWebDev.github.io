c0c60aaaeb2e25f538c3b14578a97600370b0143
kirtimisshraa <88676564+kirtimisshraa@users.noreply.github.com>
2023-03-25 20:39:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let string = "";
+let buttons = document.querySelectorAll('.numbers');
+
+Array.from(buttons).forEach((num)=>{
+    num.addEventListener('click', (e)=>{
+
+        if(e.target.innerHTML=='='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML=='C'){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else{
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+        
+    });
+
+});
